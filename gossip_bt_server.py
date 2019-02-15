d707d4dfaa2aabdc1984c3a810ebe29f5dd42b87
Bangjihee <2222wlgml@gmail.com>
2019-02-14 23:41:07 -0800

    Revise

--- gossip_bt_server.py
+++ gossip_bt_server.py
@@ -176,13 +176,13 @@ if __name__ == '__main__':
     sleep(0.5)
     neo.digitalWrite(pinNum[3], 0)
     sleep(0.5)
-    '''
+
     raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
     scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
     v = raw * scale
     t = (v - 500) / 10 - 6
     t= t*1.8 + 32
-    '''
+    
     sleep(1)
 
 
