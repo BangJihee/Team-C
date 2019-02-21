605ddf294c808263ea3e6fd921e84c6c2016261c
Bangjihee <2222wlgml@gmail.com>
2019-02-21 14:35:54 +0900

    dd

--- gossip_bt_server.py
+++ gossip_bt_server.py
@@ -391,12 +391,12 @@ while True:
                           'SN2': SN2, #O3
                           'SN3': SN3, #CO
                           'SN4': SN4, #SO2
-                          'PM25': round(PM25),
-                          'A_SN1': round(AQI_NO2),
+                          'PM25': PM25,
+                          'A_SN1': AQI_NO2,
                           'A_SN2': AQI_O3,
-                          'A_SN3': round(AQI_CO),
-                          'A_SN4': round(AQI_SO2),
-                          'A_PM25': round(AQI_PM25)
+                          'A_SN3': AQI_CO,
+                          'A_SN4': AQI_SO2,
+                          'A_PM25': AQI_PM25
 
                 }
                 msg = json.dumps(output)
