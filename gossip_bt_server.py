14d1815a9d792ee956bfe8193396f15738545aa3
Bangjihee <2222wlgml@gmail.com>
2019-02-22 05:09:33 +0900

    dd

--- gossip_bt_server.py
+++ gossip_bt_server.py
@@ -497,19 +497,29 @@ import argparse
 import asyncore
 import json
 from threading import Thread
-from time import sleep,time
+from time import sleep, time
 from datetime import datetime
 from json import dumps
 import logging
 
-#------------ Alpha sense data sheet -------------
-NO2_WE = 220; NO2_AE = 260; NO2_alpha = 0.207;
-O3_WE = 414; O3_AE = 400; O3_alpha = 0.256;
-CO_WE = 346; CO_AE = 274; CO_alpha = 0.276;
-SO2_WE = 300; SO2_AE = 394; SO2_alpha = 0.300;
-#------------------------------------------------
+# ------------ Alpha sense data sheet -------------
+NO2_WE = 220;
+NO2_AE = 260;
+NO2_alpha = 0.207;
+O3_WE = 414;
+O3_AE = 400;
+O3_alpha = 0.256;
+CO_WE = 346;
+CO_AE = 274;
+CO_alpha = 0.276;
+SO2_WE = 300;
+SO2_AE = 394;
+SO2_alpha = 0.300;
 
-def contol_mux( a, b, c, d):         # use binary bit to control mux
+
+# ------------------------------------------------
+
+def contol_mux(a, b, c, d):  # use binary bit to control mux
     neo.digitalWrite(gpiopins[0], d)
     neo.digitalWrite(gpiopins[1], c)
     neo.digitalWrite(gpiopins[2], b)
@@ -518,24 +528,27 @@ def contol_mux( a, b, c, d):         # use binary bit to control mux
     scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
     return raw, scale
 
-#---------------------------N table -------------------------------------
-#array for calculate alph
-#temp              -30,  -20   -10     0    10     20   30    40    50
-#index               0,    1,    2,    3,    4,    5,    6,    7 ,   8
-NO2_tempArray = [ 1.18, 1.18, 1.18, 1.18, 1.18, 1.18, 1.18, 2.00, 2.70] #SN1
-O3_tempArray  = [ 0.18, 0.18, 0.18, 0.18, 0.18, 0.18, 0.18, 0.18, 2.87] #SN2
-CO_tempArray  = [ 1.40, 1.03, 0.85, 0.62, 0.30, 0.03,-0.25,-0.48,-0.80] #SN3
-SO2_tempArray = [ 0.85, 0.85, 0.85, 0.85, 0.85, 1.15, 1.45, 1.75, 1.95] #SN4
-#------------------------------------------------------------------------
 
-def get_n(temper, air): #air = (NO2,O3, CO, SO2)
-    #temper
-    i=0   #index
-    mulx=0  # multiple #times
-    if(-30<=temper<-20):
+# ---------------------------N table -------------------------------------
+# array for calculate alph
+# temp              -30,  -20   -10     0    10     20   30    40    50
+# index               0,    1,    2,    3,    4,    5,    6,    7 ,   8
+NO2_tempArray = [1.18, 1.18, 1.18, 1.18, 1.18, 1.18, 1.18, 2.00, 2.70]  # SN1
+O3_tempArray = [0.18, 0.18, 0.18, 0.18, 0.18, 0.18, 0.18, 0.18, 2.87]  # SN2
+CO_tempArray = [1.40, 1.03, 0.85, 0.62, 0.30, 0.03, -0.25, -0.48, -0.80]  # SN3
+SO2_tempArray = [0.85, 0.85, 0.85, 0.85, 0.85, 1.15, 1.45, 1.75, 1.95]  # SN4
+
+
+# ------------------------------------------------------------------------
+
+def get_n(temper, air):  # air = (NO2,O3, CO, SO2)
+    # temper
+    i = 0  # index
+    mulx = 0  # multiple #times
+    if (-30 <= temper < -20):
         i = 0;
         mulx = temper + 30  # ex -28'C + 30 = 2 >> 2
-    elif(-20<=temper<-10):
+    elif (-20 <= temper < -10):
         i = 1;
         mulx = temper + 20
     elif (-10 <= temper < 0):
@@ -546,72 +559,75 @@ def get_n(temper, air): #air = (NO2,O3, CO, SO2)
         mulx = temper
     elif (10 <= temper < 20):
         i = 4;
-        mulx = temper -10
+        mulx = temper - 10
     elif (20 <= temper < 30):
         i = 5;
-        mulx = temper -20
+        mulx = temper - 20
     elif (30 <= temper < 40):
         i = 6;
-        mulx = temper -30
+        mulx = temper - 30
     elif (40 <= temper < 50):
         i = 7;
         mulx = temper - 40
     elif (50 <= temper):
-        i = 8; # if temperature exceed 50 just give 50'C data
+        i = 8;  # if temperature exceed 50 just give 50'C data
 
-    N =0.0
-    if(air == 'O3'):
-        if(i==8):
-            N=O3_tempArray[i]
+    N = 0.0
+    if (air == 'O3'):
+        if (i == 8):
+            N = O3_tempArray[i]
         else:
-            tmp=( O3_tempArray[i + 1] - O3_tempArray[i] ) / 10.0
-            N=O3_tempArray[i] + (tmp * mulx)
+            tmp = (O3_tempArray[i + 1] - O3_tempArray[i]) / 10.0
+            N = O3_tempArray[i] + (tmp * mulx)
 
-    elif(air == 'CO'):
-        if(i==8):
-            N=CO_tempArray[i]
+    elif (air == 'CO'):
+        if (i == 8):
+            N = CO_tempArray[i]
         else:
-            tmp=( CO_tempArray[i + 1] - CO_tempArray[i] ) / 10.0
-            N=CO_tempArray[i] + (tmp * mulx)
+            tmp = (CO_tempArray[i + 1] - CO_tempArray[i]) / 10.0
+            N = CO_tempArray[i] + (tmp * mulx)
 
-    elif(air == 'NO2'):
-        if(i==8):
-            N=NO2_tempArray[i]
+    elif (air == 'NO2'):
+        if (i == 8):
+            N = NO2_tempArray[i]
         else:
-            tmp=( NO2_tempArray[i + 1] - NO2_tempArray[i] ) / 10.0
-            N=NO2_tempArray[i] + (tmp * mulx)
+            tmp = (NO2_tempArray[i + 1] - NO2_tempArray[i]) / 10.0
+            N = NO2_tempArray[i] + (tmp * mulx)
 
     elif (air == 'SO2'):
-        if(i==8):
-            N=SO2_tempArray[i]
+        if (i == 8):
+            N = SO2_tempArray[i]
         else:
-            tmp=( SO2_tempArray[i + 1] - SO2_tempArray[i] ) / 10.0
-            N=SO2_tempArray[i] + (tmp * mulx)
+            tmp = (SO2_tempArray[i + 1] - SO2_tempArray[i]) / 10.0
+            N = SO2_tempArray[i] + (tmp * mulx)
 
     return N
 
-#--------------------------- AQI table ----------------------------------------
-#AQI              0-50,  51-100, 101-150, 151-200, 201-300, 301-400, 401-500
-#index               0,       1,       2,       3,       4,       5,       6,
-#MAX (03, PM25, CO, SO2, NO2, AQI)
-O3_AqiArray = [55.0, 71.0, 86.0, 106.0, 200.0,  0.0,  0.0]
-PM25_MaxAqiArray  = [12.1, 35.5, 55.5, 150.5, 250.5, 350.5, 500.4]
-CO_MaxAqiArray    = [4.5, 9.5, 12.5, 15.5, 30.5, 40.5, 50.4]
-SO2_MaxAqiArray   = [36.0, 76.0, 186.0, 305.0, 605.0, 805.0, 1004.0]
-NO2_MaxAqiArray   = [54.0, 101.0, 361.0, 650.0, 1250.0, 1650.0, 2049.0]
-Aqi_MaxAqiArray   = [51.0, 101.0, 151.0, 201.0, 301.0, 401.0, 500.0]
 
-#MIN (03, PM25, CO, SO2, NO2, AQI)
-O3_AqiArray  = [0.0, 55.0, 71.0, 86.0, 106.0, 0.0, 0.0]
-PM25_MinAqiArray  = [0.0, 12.1, 35.5, 55.5, 150.5, 250.5, 350.5]
-CO_MinAqiArray    = [0.0, 4.5, 9.5, 12.5, 15.5, 30.5, 40.5]
-SO2_MinAqiArray   = [0.0, 36.0, 76.0, 186.0, 305.0, 605.0, 805.0]
-NO2_MinAqiArray   = [0.0, 54.0, 101.0, 361.0, 650.0, 1250.0, 1650.0]
-Aqi_MinAqiArray   = [0.0, 51.0, 101.0, 151.0, 201.0, 301.0, 401.0]
-#-------------------------------------------------------------------------------
+# --------------------------- AQI table ----------------------------------------
+# AQI              0-50,  51-100, 101-150, 151-200, 201-300, 301-400, 401-500
+# index               0,       1,       2,       3,       4,       5,       6,
+# MAX (03, PM25, CO, SO2, NO2, AQI)
+O3_AqiArray = [55.0, 71.0, 86.0, 106.0, 200.0, 0.0, 0.0]
+PM25_MaxAqiArray = [12.1, 35.5, 55.5, 150.5, 250.5, 350.5, 500.4]
+CO_MaxAqiArray = [4.5, 9.5, 12.5, 15.5, 30.5, 40.5, 50.4]
+SO2_MaxAqiArray = [36.0, 76.0, 186.0, 305.0, 605.0, 805.0, 1004.0]
+NO2_MaxAqiArray = [54.0, 101.0, 361.0, 650.0, 1250.0, 1650.0, 2049.0]
+Aqi_MaxAqiArray = [51.0, 101.0, 151.0, 201.0, 301.0, 401.0, 500.0]
+
+# MIN (03, PM25, CO, SO2, NO2, AQI)
+O3_AqiArray = [0.0, 55.0, 71.0, 86.0, 106.0, 0.0, 0.0]
+PM25_MinAqiArray = [0.0, 12.1, 35.5, 55.5, 150.5, 250.5, 350.5]
+CO_MinAqiArray = [0.0, 4.5, 9.5, 12.5, 15.5, 30.5, 40.5]
+SO2_MinAqiArray = [0.0, 36.0, 76.0, 186.0, 305.0, 605.0, 805.0]
+NO2_MinAqiArray = [0.0, 54.0, 101.0, 361.0, 650.0, 1250.0, 1650.0]
+Aqi_MinAqiArray = [0.0, 51.0, 101.0, 151.0, 201.0, 301.0, 401.0]
 
 
-def AQI_convert( c , air):
+# -------------------------------------------------------------------------------
+
+
+def AQI_convert(c, air):
     c_low = 0.0
     c_high = 0.0
     i_low = 0.0
@@ -620,11 +636,11 @@ def AQI_convert( c , air):
 
     if (air == 'PM25'):
         for i in range(0, 7):
-            if(PM25_MaxAqiArray[6] < c):
-                I=500
+            if (PM25_MaxAqiArray[6] < c):
+                I = 500
                 break;
 
-            elif ( PM25_MinAqiArray[i] <= c < PM25_MaxAqiArray[i]):
+            elif (PM25_MinAqiArray[i] <= c < PM25_MaxAqiArray[i]):
                 c_low = PM25_MinAqiArray[i];
                 c_high = PM25_MaxAqiArray[i];
                 i_low = Aqi_MinAqiArray[i];
@@ -637,7 +653,7 @@ def AQI_convert( c , air):
                 I = 500
                 break;
 
-            elif ( CO_MinAqiArray[i] <= c < CO_MaxAqiArray[i]):
+            elif (CO_MinAqiArray[i] <= c < CO_MaxAqiArray[i]):
                 c_low = CO_MinAqiArray[i];
                 c_high = CO_MaxAqiArray[i];
                 i_low = Aqi_MinAqiArray[i];
@@ -649,7 +665,7 @@ def AQI_convert( c , air):
                 I = 500
                 break;
 
-            elif ( SO2_MinAqiArray[i] <= c < SO2_MaxAqiArray[i]):
+            elif (SO2_MinAqiArray[i] <= c < SO2_MaxAqiArray[i]):
                 c_low = SO2_MinAqiArray[i];
                 c_high = SO2_MaxAqiArray[i];
                 i_low = Aqi_MinAqiArray[i];
@@ -661,7 +677,7 @@ def AQI_convert( c , air):
                 I = 500
                 break;
 
-            if ( NO2_MinAqiArray[i] <= c < NO2_MaxAqiArray[i]):
+            if (NO2_MinAqiArray[i] <= c < NO2_MaxAqiArray[i]):
                 c_low = NO2_MinAqiArray[i];
                 c_high = NO2_MaxAqiArray[i];
                 i_low = Aqi_MinAqiArray[i];
@@ -673,17 +689,18 @@ def AQI_convert( c , air):
                 I = 500
                 break;
 
-            if ( O3_AqiArray[i] <= c < O3_AqiArray[i]):
+            if (O3_AqiArray[i] <= c < O3_AqiArray[i]):
                 c_low = O3_AqiArray[i];
                 c_high = O3_AqiArray[i];
                 i_low = Aqi_MinAqiArray[i];
                 i_high = Aqi_MaxAqiArray[i];
                 break;
-#AQI equation
-    if(I!=500):
+    # AQI equation
+    if (I != 500):
         I = (((i_high - i_low) / (c_high - c_low)) * (c - c_low)) + i_low
 
     return I;
+
 
 ##main start##
 if __name__ == '__main__':
@@ -704,27 +721,25 @@ if __name__ == '__main__':
     server_thread.daemon = True
     server_thread.start()
 
-    #epochtime = datetime.now().strftime('%Y-%m-%d %H:%M:%S') #(int)(time())
+    # epochtime = datetime.now().strftime('%Y-%m-%d %H:%M:%S') #(int)(time())
 
-    neo =Gpio()
+    neo = Gpio()
 
-    #S0 = 24 # pin to use
-    #S1 = 25
-    #S2 = 26
-    #S3 = 27
+    # S0 = 24 # pin to use
+    # S1 = 25
+    # S2 = 26
+    # S3 = 27
     gpiopins = [24, 25, 26, 27]
     gpio = Gpio()
 
     pinnum = [0, 0, 0, 0]
 
-
     # Set GPIO pins to output
-    #try:
+    # try:
     #   for pin in gpiopins:
     #       gpio.pinMode(pin, gpio.OUTPUT)
-    #except Exception as e:
+    # except Exception as e:
     #    logger.error("Error : GPIO pin {} .reason {}".format(pin, e.message))
-
 
     # Blink example
     for i in range(4):
@@ -732,24 +747,23 @@ if __name__ == '__main__':
 
     while True:
         for client_handler in server.active_client_handlers.copy():
-
             epochtime = datetime.now()
 
-            #c0 temperature
-            raw, scale =contol_mux(0,0,0,0)
+            # c0 temperature
+            raw, scale = contol_mux(0, 0, 0, 0)
             sleep(1)
             v = raw * scale
-            t = (v - 500)/10 - 6
+            t = (v - 500) / 10 - 6
             t = (t * 1.8) + 32
             print("temp: {} F".format(t))
 
-            #C2 NO2_we
-            raw, scale = contol_mux(0,0,1,0)
+            # C2 NO2_we
+            raw, scale = contol_mux(0, 0, 1, 0)
             sleep(0.05)
             c2 = raw * scale
 
-            #C3 NO2_ae
-            raw, scale =contol_mux(0,0,1,1)
+            # C3 NO2_ae
+            raw, scale = contol_mux(0, 0, 1, 1)
             sleep(0.05)
             c3 = raw * scale
 
@@ -758,16 +772,13 @@ if __name__ == '__main__':
             SN1 = SN1 if (SN1 >= 0) else -SN1
             print("NO2 _SN1 : {}".format(SN1))
 
-
-
-
             # C4 O3_we
-            raw, scale =contol_mux(0,1,0,0)
+            raw, scale = contol_mux(0, 1, 0, 0)
             sleep(0.05)
             c4 = raw * scale
 
             # C5 O3_ae
-            raw, scale =contol_mux(0,1,0,1)
+            raw, scale = contol_mux(0, 1, 0, 1)
             sleep(0.05)
             c5 = raw * scale
 
@@ -776,33 +787,29 @@ if __name__ == '__main__':
             SN2 = SN2 if (SN2 >= 0) else -SN2
             print("O3 _SN2 : {}".format(SN2))
 
-
-
-            #C6 CO_wE
-            raw, scale =contol_mux(0,1,1,0)
+            # C6 CO_wE
+            raw, scale = contol_mux(0, 1, 1, 0)
             sleep(0.05)
             c6 = raw * scale
 
-            #C7 CO_aE
-            raw, scale = contol_mux(0,1,1,1)
+            # C7 CO_aE
+            raw, scale = contol_mux(0, 1, 1, 1)
             sleep(0.05)
             c7 = raw * scale
 
             # SN3 CO
             SN3 = ((c6 - CO_WE) - (get_n(t, 'CO') * (c7 - CO_AE))) / CO_alpha
-            SN3 = SN3/1000
+            SN3 = SN3 / 1000
             SN3 = SN3 if (SN3 >= 0) else -SN3
             print("CO_SN3 : {}".format(SN3))
 
-
-
-            #C8 SO2_we
-            raw,scale = contol_mux(1,0,0,0)
+            # C8 SO2_we
+            raw, scale = contol_mux(1, 0, 0, 0)
             sleep(0.05)
             c8 = raw * scale
 
-            #C9 SO2_aE
-            raw, scale = contol_mux(1,0,0,1)
+            # C9 SO2_aE
+            raw, scale = contol_mux(1, 0, 0, 1)
             sleep(0.05)
             c9 = raw * scale
 
@@ -811,17 +818,18 @@ if __name__ == '__main__':
             SN4 = SN4 if (SN4 >= 0) else -SN4
             print("SO2_SN4 : {}".format(SN4))
 
-
-            #c11 PM2.5
-            raw, scale = contol_mux(1,0,1,1)
+            # c11 PM2.5
+            raw, scale = contol_mux(1, 0, 1, 1)
             sleep(0.05)
             c11 = (raw * scale) / 1000
 
-            #PM2.5 equation
+            # PM2.5 equation
 
-            hppcf = (240.0 * pow(c11, 6) - 2491.3 * pow(c11, 5) + 9448.7 * pow(c11, 4) - 14840.0 * pow(c11, 3) + 10684.0 * pow(c11, 2) + 2211.8 * c11 + 7.9623)
+            hppcf = (240.0 * pow(c11, 6) - 2491.3 * pow(c11, 5) + 9448.7 * pow(c11, 4) - 14840.0 * pow(c11,
+                                                                                                       3) + 10684.0 * pow(
+                c11, 2) + 2211.8 * c11 + 7.9623)
             PM25 = 0.518 + .00274 * hppcf
-            PM25= PM25
+            PM25 = PM25
             AQI_PM25 = AQI_convert(PM25, 'PM25')
             print("PM25 : {}".format(PM25))
             print("\n")
@@ -837,38 +845,40 @@ if __name__ == '__main__':
             print("AQI_SO2 : {}".format(int(AQI_SO2)))
             print("AQI_PM25: {}".format(int(AQI_PM25)))
 
-            nowtime = datetime.datetime.now()
+        nowtime = datetime.now()
 
-            if args.output_format == "json":
-                output = {
-                    "year": nowtime.year,
-                    "month": nowtime.month,
-                    "day": nowtime.day,
-                    "hour": nowtime.hour,
-                    "minute": nowtime.minute,
-                    "second": nowtime.second,
-                    "temp": round(t),
-                    'SN1': SN1,  # NO2
-                    'SN2': SN2,  # O3
-                    'SN3': SN3,  # CO
-                    'SN4': SN4,  # SO2
-                    'PM25': PM25,
-                    'A_SN1': AQI_NO2,
-                    'A_SN2': AQI_O3,
-                    'A_SN3': AQI_CO,
-                    'A_SN4': AQI_SO2,
-                    'A_PM25': AQI_PM25
+        if args.output_format == "json":
+            output = {
+                "year": nowtime.year,
+                "month": nowtime.month,
+                "day": nowtime.day,
+                "hour": nowtime.hour,
+                "minute": nowtime.minute,
+                "second": nowtime.second,
+                "temp": round(t),
+                'SN1': SN1,  # NO2
+                'SN2': SN2,  # O3
+                'SN3': SN3,  # CO
+                'SN4': SN4,  # SO2
+                'PM25': PM25,
+                'A_SN1': AQI_NO2,
+                'A_SN2': AQI_O3,
+                'A_SN3': AQI_CO,
+                'A_SN4': AQI_SO2,
+                'A_PM25': AQI_PM25
 
-                }
+            }
 
-                msg = json.dumps(output)
-            elif args.output_format == "csv":
-                msg = "Time:{}, {}, {}, {}, {}, {}, {}, {} ,{}, {}, {} , {} ".format(datetime, t, SN1, SN2, SN3, SN4,AQI_PM25, AQI_NO2, AQI_O3, AQI_CO, AQI_SO2, AQI_PM25)
-            try:
-                client_handler.send((msg + '\n').encode('ascii'))
-            except Exception as e:
-                BTError.print_error(handler=client_handler, error=BTError.ERR_WRITE, error_message=repr(e))
-                client_handler.handle_close()
+            msg = json.dumps(output)
+        elif args.output_format == "csv":
+            msg = "Time:{}, {}, {}, {}, {}, {}, {}, {} ,{}, {}, {} , {} ".format(datetime, t, SN1, SN2, SN3, SN4,
+                                                                                 AQI_PM25, AQI_NO2, AQI_O3, AQI_CO,
+                                                                                 AQI_SO2, AQI_PM25)
+        try:
+            client_handler.send((msg + '\n').encode('ascii'))
+        except Exception as e:
+            BTError.print_error(handler=client_handler, error=BTError.ERR_WRITE, error_message=repr(e))
+            client_handler.handle_close()
 
         # Sleep for 5 seconds
-        sleep(5)
+        sleep(2.5)
