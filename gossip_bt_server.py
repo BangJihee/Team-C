a087def30068f639b69d63c3d8a502dc505c3c36
Bangjihee <2222wlgml@gmail.com>
2019-02-22 11:14:23 +0900

    dd

--- gossip_bt_server.py
+++ gossip_bt_server.py
@@ -257,11 +257,7 @@ if __name__ == '__main__':
             sleep(0.05)
             c3 = raw * scale
 
-            print("------------------------------------------")
-            print("c2 c3")
-            print(c2)
-            print(c3)
-            print("------------------------------------------")
+
 
             # SN1= NO2
             SN1 = ((c2 - NO2_WE) - (get_n(t, 'NO2') * (c3 - NO2_AE))) / NO2_alpha
@@ -279,11 +275,7 @@ if __name__ == '__main__':
             sleep(0.05)
             c5 = raw * scale
 
-            print("------------------------------------------")
-            print("c4 c5")
-            print(c4)
-            print(c5)
-            print("------------------------------------------")
+
             # SN2 =O3
             SN2 = ((c4 - O3_WE) - (get_n(t, 'O3') * (c5 - O3_AE))) / O3_alpha
             SN2 = SN2 if (SN2 >= 0) else -SN2
@@ -299,11 +291,7 @@ if __name__ == '__main__':
             raw, scale = contol_mux(0, 1, 1, 1)
             sleep(0.05)
             c7 = raw * scale
-            print("------------------------------------------")
-            print("c6 c7")
-            print(c6)
-            print(c7)
-            print("------------------------------------------")
+
             # SN3= CO
             SN3 = ((c6 - CO_WE) - (get_n(t, 'CO') * (c7 - CO_AE))) / CO_alpha
             SN3 = SN3 / 1000
@@ -321,11 +309,6 @@ if __name__ == '__main__':
             sleep(0.05)
             c9 = raw * scale
 
-            print("------------------------------------------")
-            print("c8 c9")
-            print(c8)
-            print(c9)
-            print("------------------------------------------")
 
             #  SN4= SO2
             SN4 = ((c8 - SO2_WE) - (get_n(t, 'SO2') * (c9 - SO2_AE))) / SO2_alpha
